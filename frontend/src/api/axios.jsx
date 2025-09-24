f784877276797bbeb648f03f76f7cae2a6e7e14c
Nakul <nakultalsaniya6992@gmail.com>
2025-09-25 01:56:46 +0530

    backend url done

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 //create axios instance with base url
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://nakuls-restaurant.onrender.com',
   withCredentials: true,
 });
 export default instance;
